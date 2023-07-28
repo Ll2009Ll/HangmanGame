6b99e1e06055a984c89a67315f5475c326cc31c5
Ll2009Ll <140837963+Ll2009Ll@users.noreply.github.com>
2023-07-28 20:36:41 +0400

    Add files via upload

---
+++ ogn.js
@@ -0,0 +1,131 @@
+/*let a = prompt("a arjeq")
+if(a %2  == 0){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+let a1 = 20;
+let b1 = 25;
+if(a1 % 2 == 0){
+    console.log("es zuyg em")
+}
+else if (b1 % 2 == 0) {
+    console.log("es zuyg em")
+}
+else{
+    console.log("menq zuyg chenq")
+}
+
+let a1 = prompt("a1i arjeq")
+let b1 = prompt("b1-i arjeq");
+if(a1 % 2 == 0){
+    console.log("es zuyg em")
+}
+else if (b1 % 2 == 0) {
+    console.log("es zuyg em")
+}
+else{
+    console.log("menq zuyg chenq")
+}*/
+/*   push     ,pop      ,unshift  ,shift   ,   */
+
+/*
+let a = []
+
+console.log()*/
+
+/*
+for( let a = 20 ; a <= 35; a++){
+    if(a%2 == 0){
+        console.log(a)
+    }
+    else(
+        console.log("es zuyg chem")
+    )
+}*/
+/*
+for(let a = 0; a <=5;a++){
+    if(a%2!==0){
+        console.log(`${a},es kent em`)
+    }
+    else if (a==0){
+        console.log(`${a},es 0 em`)
+    }
+    else{
+        console.log(`${a},es zuyg em`)
+
+    }
+}*/
+/*
+let a = [1,2,"aaa",[false,true],"eee",45,5,6,"aaa"]
+
+for(let i = 0;i<= a.length;i++){
+    console.log(a[i])
+}
+*/
+/*
+let b = [];
+let a=[];
+let i = []
+let arr=[1,2,3,4,5,6,7,8,9]
+for(i=1;i<=arr.length;i++){
+    if(i%2 === 0){
+        b.push(i)
+        
+    }
+    else{
+        a.push(i)
+        
+    }
+}
+console.log(a)
+console.log(b)
+*/
+
+/* 
+Math.random ->0-1 -> 0.1357784254;       Math.floor(Math.random()*100)
+Math.floor(2.9)->3;
+Math.ceil(4.1)-> 5;
+Math.round(5.5)-> 6
+Math.pow(5 ,3 )-> 5*5*5 ->125
+Math.sqrt(25)-> 5 */
+/*
+function x(){
+    let a = prompt("a")
+    console.log(a*a)
+}
+x()*/
+/*
+let arr = ["a","b","g","d","e","z","u","y","t","j","i","l"]
+function b(a){
+    let x = Math.floor(Math.random()*a.length);
+    return a[x]
+}
+console.log(b(arr))
+let levon = {
+    name: "Levon",
+    age:14,
+    teacher:"Alen Mkhitaryan",
+    hair:"black",
+    hello(){
+        console.log(`Hello my name is ${this.name}`)
+    },
+    yourName(){
+        console.log("what is your name")
+    }
+}
+
+
+console.log(levon.name)
+console.log(levon.age)
+console.log(levon.teacher)
+console.log(levon.hair)
+levon.hello();
+levon.yourName();
+console.log(levon.name)*/
+
+/*.lose-msg:hover {
+    color: #fe5152;
+    }*/
